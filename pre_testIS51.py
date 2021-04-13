408a8d57e13d58eda70faf83564bb8341990960e
Umbasa <umbasa@Umbasas-MacBook-Air.local>
2021-04-13 10:44:23 -0700

    fCompleted Structured-English

---
+++ pre_testIS51.py
@@ -0,0 +1,19 @@
+"""
+Structured-English
+This program is trying to determine which payment option is better (more money).
+First Option pays $100 per day for 10 days. The second option pays $1 the first day, $2 the second day, $4 the third day, and 
+continues to double for each day until 10 days. There will be two functions to calculate the pay rate.
+
+function1 will calculate the amount for the first option. Function2 will calculate the amount for the second option. 
+
+function1 will output 100 per days * 10 days. 
+
+function2 will loop 10 times, with each time, doubling the amount and add the amount to the total.
+
+If the amount is equal, we output to the user " Option1 and Option2 are the same"
+If option1 is better, we output to the user "Option 1 is better"
+If option2 is better, we output to the user "Option 2 is better"
+
+"""
+
+
