442ab7bc6a93f5e91055ad6016137cec60247f8f
Umbasa <umbasa@Umbasas-MacBook-Air.local>
2021-04-13 10:57:00 -0700

    Completed Pseudo-Code

--- pre_testIS51.py
+++ pre_testIS51.py
@@ -16,4 +16,30 @@ If option2 is better, we output to the user "Option 2 is better"
 
 """
 
+"""
+Pseudo-code
 
+# option1 
+return 100 * 10
+
+#option2
+amount = 1
+list1 = []
+loop 10 times
+  add amount to list1
+  amount *= 2
+  sum = sum of all items in the loop
+return sum
+#main
+var1 = option1
+va2 = option2
+
+if var 1 == var2
+    "Option 1 and Option 2 pays the same."
+if var1 < var2
+    "Option 2 is better."
+else 
+    "Option 1 is better."
+
+main
+"""
